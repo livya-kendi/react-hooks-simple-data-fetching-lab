1b0b4df5423adb3fb92ea52381dabf0cb8e956fd
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-23 21:46:11 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+
+    const [randomDog, setRandomDog] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res)=> res.json())
+        .then((data)=> { 
+            setRandomDog(data.message);
+            setIsLoaded(true);
+        });
+    }, []);
+
+    console.log(randomDog)
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return <img src={randomDog} alt="A Random Dog" />
+}
+
+export default App
